10fde5f95e8a2db092f3dc51c00b7300dee452f0
FriendlyKirby <54104874+FriendlyKirby@users.noreply.github.com>
2024-10-11 13:57:08 -0700

    base documentation done for ActivityTrackerApp.java, and finished Main.java

---
+++ src/main/ui/ActivityTrackerApp.java
@@ -0,0 +1,43 @@
+package ui;
+
+import model.Activity;
+import model.ActivityTracker;
+import model.Session;
+
+
+// Activity tracker application
+public class ActivityTrackerApp {
+    private ActivityTracker activityTracker; // The activity tracker which is the list of activities
+    // !!!
+
+    // EFFECTS: runs the activity tracker application
+    public ActivityTrackerApp() {
+        runActivityTracker();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes user input
+    private void runActivityTracker() {
+        init();
+        // !!!
+    }
+
+    // MODIFIES: this
+    // EFFECTS: initializes activity tracker
+    private void init() {
+        // !!!
+    }
+
+    // EFFECTS: displays option menu to user
+    private void displayMainMenu() {
+        // !!!
+    }
+
+    // MODIFIES: this
+    // EFFECTS: takes in input prompt from user and proceeds with action
+    private void mainMenuInput() {
+        // !!!
+    }
+
+    
+}
