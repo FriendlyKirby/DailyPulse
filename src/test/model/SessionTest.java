787fde1244603bfd6f8b028dab7cc99e94438f34
FriendlyKirby <54104874+FriendlyKirby@users.noreply.github.com>
2024-10-11 07:36:26 -0700

    removed setDuration and added the test for session

--- src/test/model/SessionTest.java
+++ src/test/model/SessionTest.java
@@ -1,6 +1,7 @@
 package model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import java.time.LocalDate;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,11 +11,27 @@ class SessionTest {
 
     @BeforeEach
     void runBefore() {
-        testSession = new Session(0);
+        testSession = new Session(10);
     }
 
     @Test
-    void sampleTest() {
-        assertTrue(true);
+    void testConstructor() {
+        assertEquals(10, testSession.getDurationInHours());
+        assertEquals(1, testSession.getId());
+        assertEquals(LocalDate.now(), testSession.getDate());
     }
+
+    @Test
+    void testSetId() {
+        assertEquals(1, testSession.getId());
+        testSession.setId(2);
+        assertEquals(2, testSession.getId());
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("Session ID: 1, Duration: 10 hours, Date: 2024-10-11",
+        testSession.toString());
+    }
+
 }
