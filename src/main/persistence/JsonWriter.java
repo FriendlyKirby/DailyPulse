f6bfbf80cfaf77b72e495c523b7266ca323d1885
FriendlyKirby <54104874+FriendlyKirby@users.noreply.github.com>
2024-10-24 23:33:43 -0700

    implemented persistence and test for JsonWriter

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -14,30 +14,31 @@ public class JsonWriter {
 
     // EFFECTS: constructs writer to write to destination file
     public JsonWriter(String destination) {
-        // stub
+        this.destination = destination;
     }
 
     // MODIFIES: this
     // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot be opened for writing
     public void open() throws FileNotFoundException {
-        // stub
+        writer = new PrintWriter(new File(destination));
     }
 
     // MODIFIES: this
     // EFFECTS: writes JSON representation of activity tracker to file
     public void write(ActivityTracker at) {
-        // stub
+        JSONObject json = at.toJson();
+        saveToFile(json.toString(TAB));
     }
 
     // MODIFIES: this
     // EFFECTS: closes writer
     public void close() {
-        // stub
+        writer.close();
     }
 
     // MODIFIES: this
     // EFFECTS: writes string to file
     private void saveToFile(String json) {
-        // stub
+        writer.print(json);
     }
 }
--- src/test/persistence/JsonWriterTest.java
+++ src/test/persistence/JsonWriterTest.java
@@ -14,16 +14,74 @@ class JsonWriterTest extends JsonTest {
 
     @Test
     void testWriterInvalidFile() {
-        // stub
+        try {
+            ActivityTracker tracker = new ActivityTracker();
+            JsonWriter writer = new JsonWriter("./data/my\0illegal:fileName.json");
+            writer.open();
+            fail("FileNotFoundException was expected");
+        } catch (IOException e) {
+            // Expected exception caught
+        }
     }
 
     @Test
     void testWriterEmptyActivityTracker() {
-        // stub
+        try {
+            ActivityTracker tracker = new ActivityTracker();
+            JsonWriter writer = new JsonWriter("./data/testWriterEmptyActivityTracker.json");
+            writer.open();
+            writer.write(tracker);
+            writer.close();
+
+            JsonReader reader = new JsonReader("./data/testWriterEmptyActivityTracker.json");
+            tracker = reader.read();
+            assertEquals(0, tracker.getActivities().size());
+        } catch (IOException e) {
+            fail("Exception should not have been thrown");
+        }
     }
 
     @Test
     void testWriterGeneralActivityTracker() {
-        // stub
+        try {
+            ActivityTracker tracker = new ActivityTracker();
+
+            Activity activity = new Activity("Running");
+            activity.setStreak(3);
+            activity.setTotalTime(5);
+            activity.setNextSessionId(3);
+
+            Session session1 = new Session(2);
+            session1.setId(1);
+            session1.setDate(LocalDate.parse("2023-10-20"));
+            Session session2 = new Session(3);
+            session2.setId(2);
+            session2.setDate(LocalDate.parse("2023-10-21"));
+
+            activity.getSessions().add(session1);
+            activity.getSessions().add(session2);
+
+            tracker.addActivity(activity);
+
+            JsonWriter writer = new JsonWriter("./data/testWriterGeneralActivityTracker.json");
+            writer.open();
+            writer.write(tracker);
+            writer.close();
+
+            JsonReader reader = new JsonReader("./data/testWriterGeneralActivityTracker.json");
+            tracker = reader.read();
+            List<Activity> activities = tracker.getActivities();
+            assertEquals(1, activities.size());
+            Activity readActivity = activities.get(0);
+            checkActivity("Running", 3, 5, readActivity);
+
+            List<Session> readSessions = readActivity.getSessions();
+            assertEquals(2, readSessions.size());
+            checkSession(2, 1, "2023-10-20", readSessions.get(0));
+            checkSession(3, 2, "2023-10-21", readSessions.get(1));
+
+        } catch (IOException e) {
+            fail("Exception should not have been thrown");
+        }
     }
 }
