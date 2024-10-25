eae7e2afaa2caadf46a234a5c45f31392698d5e0
FriendlyKirby <54104874+FriendlyKirby@users.noreply.github.com>
2024-10-24 22:37:04 -0700

    created documentation for JsonReader.java

---
+++ src/main/persistence/JsonReader.java
@@ -0,0 +1,22 @@
+package persistence;
+
+import model.ActivityTracker;
+import org.json.JSONObject;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+// credit to JsonSerializationDemo
+// Represents a reader that reads activity tracker from JSON data stored in file
+public class JsonReader {
+    private String source;
+
+    // EFFECTS: constructs reader to read from source file
+    public JsonReader(String source) {
+        this.source = source;
+    }
+
+    // methods ...
+}
