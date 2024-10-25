6eb4e05a347fbec0d6c35e0e39b0f519634dea54
FriendlyKirby <54104874+FriendlyKirby@users.noreply.github.com>
2024-10-24 22:19:49 -0700

    added documentation and stubs to JsonWriter.java

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -14,8 +14,30 @@ public class JsonWriter {
 
     // EFFECTS: constructs writer to write to destination file
     public JsonWriter(String destination) {
-        this.destination = destination;
+        // stub
     }
 
-    // Methods will be implemented here...
+    // MODIFIES: this
+    // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot be opened for writing
+    public void open() throws FileNotFoundException {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes JSON representation of activity tracker to file
+    public void write(ActivityTracker at) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: closes writer
+    public void close() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes string to file
+    private void saveToFile(String json) {
+        // stub
+    }
 }
