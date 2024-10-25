3d83c744567c2b344e72f0ed23f4c4ffd1ac7fb7
FriendlyKirby <54104874+FriendlyKirby@users.noreply.github.com>
2024-10-24 22:50:32 -0700

    created documentation and stubs for JsonReaderTest.java

---
+++ src/test/persistence/JsonReaderTest.java
@@ -0,0 +1,28 @@
+package persistence;
+
+import model.ActivityTracker;
+import model.Activity;
+import model.Session;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+// credit to JsonSerializationDemo
+class JsonReaderTest extends JsonTest {
+
+    @Test
+    void testReaderNonExistentFile() {
+        // stub
+    }
+
+    @Test
+    void testReaderEmptyActivityTracker() {
+        // stub
+    }
+
+    @Test
+    void testReaderGeneralActivityTracker() {
+        // stub
+    }
+}
