f4832ee76ad5d100718010d914bcbdee585856ba
FriendlyKirby <54104874+FriendlyKirby@users.noreply.github.com>
2024-10-24 22:30:09 -0700

    documentation/ stubs for JsonWriterTest.java

---
+++ src/test/persistence/JsonWriterTest.java
@@ -0,0 +1,29 @@
+package persistence;
+
+import model.ActivityTracker;
+import model.Activity;
+import model.Session;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+// credit to JsonSerializationDemo
+class JsonWriterTest extends JsonTest {
+
+    @Test
+    void testWriterInvalidFile() {
+        // stub
+    }
+
+    @Test
+    void testWriterEmptyActivityTracker() {
+        // stub
+    }
+
+    @Test
+    void testWriterGeneralActivityTracker() {
+        // stub
+    }
+}
